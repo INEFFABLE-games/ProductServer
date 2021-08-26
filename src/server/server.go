56fe732de7ec38a2e76b579fe0477da2e749bea3
Anthony <ZetoX3429@mail.ru>
2021-08-26 16:36:38 +0300

    fix: changed pgx version, fixed code for pgx v4

--- src/server/server.go
+++ src/server/server.go
@@ -5,39 +5,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis/v8"
-	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v4"
 	"github.com/segmentio/kafka-go"
 	"log"
 	"main/src/internal/models"
 	"main/src/internal/repository"
 	"main/src/internal/services"
-	"os"
 	"time"
 	//"github.com/jackc/pgx"
 )
 
 func getPostgre() *pgx.Conn {
-	conn, err := pgx.Connect(pgx.ConnConfig{
-		Host:                 "localhost",
-		Port:                 5432,
-		Database:             "products",
-		User:                 "postgres",
-		Password:             "12345",
-		TLSConfig:            nil,
-		UseFallbackTLS:       false,
-		FallbackTLSConfig:    nil,
-		Logger:               nil,
-		LogLevel:             0,
-		Dial:                 nil,
-		RuntimeParams:        nil,
-		OnNotice:             nil,
-		CustomConnInfo:       nil,
-		CustomCancel:         nil,
-		PreferSimpleProtocol: false,
-		TargetSessionAttrs:   "",
-	})
+	conn, err := pgx.Connect(context.Background(), "port=5432 host=localhost user=postgres password=12345 dbname=products sslmode=disable")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		fmt.Printf("Unable to connect to database: %v\n", err)
 	}
 
 	return conn
@@ -74,45 +55,57 @@ func Start(ctx context.Context) {
 	conn := getPostgre()
 	productPostgreRepo := repository.NewPostgreProductRepository(conn)
 	productService := services.NewProductService(&productPostgreRepo)
-	defer conn.Close()
+	defer conn.Close(ctx)
 
 	//---------------------------------------------------------KAFKA
 	conf := kafka.ReaderConfig{
-		Brokers:  []string{"localhost:29092"},
-		Topic:    topic,
-		GroupID:  "g1",
-		MaxBytes: 10,
+		Brokers:        []string{"localhost:29092"},
+		Topic:          topic,
+		GroupID:        "g1",
+		MaxBytes:       10,
+		CommitInterval: 10 * time.Second,
 	}
 
-	redisClient := getRedis() //-----------------------------redis
+	//-----------------------------redis
+	redisClient := getRedis()
+	err := redisClient.Do(ctx, "DEL", "products").Err()
+	if err != nil {
+		log.Println(err)
+	}
 
+	//----------------------------kafka
 	reader := kafka.NewReader(conf)
 
 	for {
-		m, err := reader.ReadMessage(ctx)
-		if err != nil {
-			fmt.Println("Some error: " + err.Error())
-			continue
+
+		var newproducts = make([]models.Product, 10)
+
+		for i := 0; i < 10; i++ {
+			m, err := reader.ReadMessage(ctx)
+			if err != nil {
+				fmt.Println("Some error: " + err.Error())
+				continue
+			}
+			prod := models.Product{}
+
+			err = json.Unmarshal(m.Value, &prod)
+			if err != nil {
+				fmt.Println(err)
+			}
+			newproducts[i] = prod
+
+			err = redisClient.Do(ctx, "XADD", "products", "*", "name", prod.Name, "price", prod.Price).Err()
+			if err != nil {
+				log.Println(err)
+			}
+
+			fmt.Printf("new product: %v\n", prod)
 		}
 
-		prod := models.Product{}
-
-		err = json.Unmarshal(m.Value, &prod)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		err = redisClient.Do(ctx, "XADD", "products", "*", "name", prod.Name, "price", prod.Price).Err()
+		err = productService.AddProduct(newproducts)
 		if err != nil {
 			log.Println(err)
 		}
-
-		err = productService.AddProduct(prod)
-		if err != nil {
-			log.Println(err)
-		}
-
-		fmt.Printf("new product: %v\n", prod)
 
 		time.Sleep(3 * time.Second)
 	}
