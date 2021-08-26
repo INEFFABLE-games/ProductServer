310e9e3d54b089b7958e074a9a2d7ddbb644b4c0
Anthony <ZetoX3429@mail.ru>
2021-08-26 16:34:18 +0300

    feat: addded batch insert implementation

--- src/internal/repository/postgreProductsRepository.go
+++ src/internal/repository/postgreProductsRepository.go
@@ -1,7 +1,9 @@
 package repository
 
 import (
-	"github.com/jackc/pgx"
+	"context"
+	"github.com/jackc/pgx/v4"
+	"log"
 	"main/src/internal/models"
 )
 
@@ -9,11 +11,19 @@ type PostgreProductRepository struct {
 	conn *pgx.Conn
 }
 
-func (p *PostgreProductRepository) AddProduct(prod models.Product) error {
+func (p *PostgreProductRepository) AddProduct(prod []models.Product) error {
 
-	_, err := p.conn.Exec("insert into products(name,price) values($1,$2)", string(prod.Name), prod.Price)
+	b := &pgx.Batch{}
+	for _, v := range prod {
+		b.Queue("INSERT INTO products(name,price) values($1,$2)", (v.Name), v.Price)
+	}
 
-	return err
+	res := p.conn.SendBatch(context.TODO(), b)
+	err := res.Close()
+	if err != nil {
+		log.Println(err)
+	}
+	return nil
 }
 
 func NewPostgreProductRepository(conn *pgx.Conn) PostgreProductRepository {
